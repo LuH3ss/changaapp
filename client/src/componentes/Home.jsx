a0b73f03c7c37fec0852fc19fb433fb293f067a6
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-08-19 20:08:25 -0300

    Render nombre service en Card

--- client/src/componentes/Home.jsx
+++ client/src/componentes/Home.jsx
@@ -30,13 +30,11 @@ export default function Home() {
   const handleClick = (e) => {
     logout();
     navigate("/");
-
   };
 
   const handleSort = (e) => {
     setOrder(e.target.value);
     dispatch(sortServices(e.target.value));
-
   };
 
   console.log(allServices);
@@ -78,7 +76,7 @@ export default function Home() {
                   img={service.img ? service.img : imgDef}
                   description={service.description}
                   price={service.price}
-                  category={service.categories[0].name}
+                  category={service.categories[0]?.name}
                 />
               </Link>
             );
