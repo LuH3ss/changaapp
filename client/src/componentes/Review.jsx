c7a4f6e5beecf251e98216b82620971281e9d042
enriquegomeznaar <enriquegomeznaar@gmail.com>
2022-09-01 12:29:25 -0300

    starRating

--- client/src/componentes/Review.jsx
+++ client/src/componentes/Review.jsx
@@ -1,40 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { FaStar } from "react-icons/fa";
-import "./css/review.css"
+import "./css/review.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserEmail } from "../redux/actions";
+import { useParams } from "react-router-dom";
+
 export default function Review({ user_id }) {
-  const { rating, setRating } = useState();
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  const [message, setMessage] = useState("");
+  const userData = useSelector((state) => state.filter);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUserEmail());
+  }, [dispatch]);
+  console.log(userData);
   const postReview = () => {
     axios.post("http://www.localhost:3001/reviews", {
-      message: "Excelente",
-      rate: 5,
-      user_id: "408d32af-1e4b-4bfa-a04d-8ed9b71b1490",
-      author_id: "ac9b8aaf-0da8-4e14-8e6a-d6e00349426e",
+      message: message,
+      rate: rating,
+      user_id: id,
+      author_id: userData.user_id,
     });
   };
+
   return (
     <div>
-      <h1>Componente review</h1>
-      <h3>Reseña:</h3>
+      <h3>
+        Message: {message} <input onChange={() => setMessage()} type="text" />
+      </h3>
       {[...Array(5)].map((star, i) => {
         const ratingValue = i + 1;
+
         return (
           <div className="reviews">
             <label>
               <input
-              className="input"
+                className="input"
                 type="radio"
                 name="rating"
                 value={ratingValue}
                 onClick={() => setRating(ratingValue)}
               />
-              <FaStar className="star" size={100} 
-              color={ratingValue <= rating ? "#ffc107" : "#e4e5e9"}/>;
+              <FaStar
+                className="star"
+                color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                size={100}
+                onMouseEnter={() => setHover(ratingValue)}
+                onMouseLeave={() => setHover(null)}
+              />
+              ;
             </label>
           </div>
         );
-    })}
-    <button onClick={postReview}>Enviar</button>
+      })}
+      <button onClick={postReview}>Enviar</button>
     </div>
   );
 }
