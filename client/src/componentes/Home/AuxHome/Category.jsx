11006beedc6e74edad47198cbd494bf8784e7548
enriquegomeznaar <enriquegomeznaar@gmail.com>
2022-09-04 15:18:22 -0300

    style

--- client/src/componentes/Home/AuxHome/Category.jsx
+++ client/src/componentes/Home/AuxHome/Category.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getAllCategories } from "../../../redux/actions";
 
+import "../../css/home.css";
 export default function Category() {
   const category = useSelector((state) => state.categories);
 
@@ -16,28 +17,33 @@ export default function Category() {
 
   return (
     <>
-      <h4>Categorias mas concurridas</h4>
-      <Box sx={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+      <Typography
+        variant="h4"
+        sx={{
+          position: "relative ",
+          margin: "60px auto 40px",
+          borderBottom: "solid 2px black",
+          paddingBottom: "40px",
+          width: "700px",
+        }}
+      >
+        Categorias mas concurridas
+      </Typography>
+      <div className="card-category-container">
         {category &&
           category?.map((e) => {
+            console.log(category)
             return (
               <Link key={e.id} to={`/home/${e.name}`}>
-                <div
-                  style={{
-                    margin: "50px",
-                    border: "solid black 1px",
-                    maxWidth: "370px",
-                    textAlign: "center",
-                  }}
-                >
+                <div className="card-category-container">
                   <h3>{e.name}</h3>
-                  <img src={e.img} alt={e.name} width="350px" height="250px" />
+                  <img src={e.img} alt={e.name} width="80%" height="250px" />
                   {/* <Button value={e.name} onClick={handleOnClick}>Ir</Button> */}
                 </div>
               </Link>
             );
           })}
-      </Box>
+      </div>
     </>
   );
 }
