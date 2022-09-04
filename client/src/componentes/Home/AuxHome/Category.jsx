4b9980a00e1fc272d75adfe9051813086bb6df2a
enriquegomeznaar <enriquegomeznaar@gmail.com>
2022-09-04 18:01:50 -0300

    style

--- client/src/componentes/Home/AuxHome/Category.jsx
+++ client/src/componentes/Home/AuxHome/Category.jsx
@@ -2,10 +2,11 @@
 import { Box, Button, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { getAllCategories } from "../../../redux/actions";
 
 import "../../css/home.css";
+
 export default function Category() {
   const category = useSelector((state) => state.categories);
 
@@ -16,31 +17,35 @@ export default function Category() {
   }, [dispatch]);
 
   return (
-    <>
+    <><div className="typography">
       <Typography
         variant="h4"
         sx={{
           position: "relative ",
-          margin: "60px auto 40px",
+          margin: "auto",
           borderBottom: "solid 2px black",
-          paddingBottom: "40px",
-          width: "700px",
+          paddingBottom: "30px",
+          width: "500px",
+          padding:"40px"
         }}
       >
         Categorias mas concurridas
       </Typography>
+      </div>
       <div className="card-category-container">
         {category &&
           category?.map((e) => {
-            console.log(category)
+            console.log(category);
             return (
-              <Link key={e.id} to={`/home/${e.name}`}>
-                <div className="card-category-container">
-                  <h3>{e.name}</h3>
-                  <img src={e.img} alt={e.name} width="80%" height="250px" />
-                  {/* <Button value={e.name} onClick={handleOnClick}>Ir</Button> */}
+              <NavLink key={e.id} to={`/home/${e.name}`}>
+                <div >
+                  <div className="card-category"  >
+                    <h3 className="title-category">{e.name}</h3>
+                    <img src={e.img} alt={e.name} height="200px" width="300px" />
+                    {/* <Button value={e.name} onClick={handleOnClick}>Ir</Button> */}
+                  </div>
                 </div>
-              </Link>
+              </NavLink>
             );
           })}
       </div>
--- client/src/componentes/reviews/Reviews.jsx
+++ client/src/componentes/reviews/Reviews.jsx
@@ -19,7 +19,7 @@ export default function Reviews() {
         allReviews && allReviews.map(rev => {
           return (
             <Box className='reviewCard' key={rev.id}>
-              <Avatar sx={{ width: 152, height: 152, bottom:'80px', marginBottom:'-50px' }}>
+              <Avatar sx={{ width: 152, height: 152, bottom:'80px', marginBottom:'-50px', boxShadow: "rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px" }}>
             {rev.user?.img ? (
               <img src={rev.user?.img} alt="?" width="152px" height="152px" />
             ) : (
